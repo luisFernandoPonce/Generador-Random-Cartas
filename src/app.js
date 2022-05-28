b3a6887d61371db00d8a5d7ad7d955f4b4a0b539
Luis Ponce <luiferponce@gmail.com>
2022-05-28 22:05:00 +0000

    generador de cartas

--- src/app.js
+++ src/app.js
@@ -7,5 +7,51 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  generadorCarta();
+  generadorSimbolo();
 };
+
+function generadorCarta() {
+  var number = document.getElementById("number");
+  var num = Math.floor(Math.random() * 13) + 1;
+  switch (num) {
+    case 1:
+      num = "A";
+      break;
+    case 11:
+      num = "J";
+      break;
+    case 12:
+      num = "Q";
+      break;
+    case 13:
+      num = "K";
+      break;
+  }
+  number.innerHTML = num;
+  return number;
+}
+
+function generadorSimbolo() {
+  let carta = ["♥", "♣", "♦", "♠"];
+  let color = ["red", "black"];
+  let selCarta = random(carta);
+
+  let selColor = random(color);
+  selColor = color[selColor];
+
+  var simboloHeader = document.getElementById("header");
+  simboloHeader.innerHTML = carta[selCarta];
+  simboloHeader.setAttribute("style", "color:" + selColor);
+  var simboloFooter = document.getElementById("footer");
+  simboloFooter.innerHTML = carta[selCarta];
+  simboloFooter.setAttribute("style", "color:" + selColor);
+
+  return simboloHeader, simboloFooter;
+}
+
+function random(array) {
+  var selection = Math.floor(Math.random() * array.length);
+  return selection;
+}
